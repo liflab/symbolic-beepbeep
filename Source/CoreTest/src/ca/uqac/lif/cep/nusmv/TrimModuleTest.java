fced54bfd7bc5a4418e11ad1297ff4c2325f408b
Sylvain Hallé <shalle@acm.org>
2022-06-03 12:22:16 -0400

    Added test for Trim

--- Source/CoreTest/src/ca/uqac/lif/cep/nusmv/TrimModuleTest.java
+++ Source/CoreTest/src/ca/uqac/lif/cep/nusmv/TrimModuleTest.java
@@ -2,6 +2,8 @@ package ca.uqac.lif.cep.nusmv;
 
 import static org.junit.Assert.*;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import ca.uqac.lif.nusmv4j.Assignment;
@@ -81,5 +83,21 @@ public class TrimModuleTest
 		assertEquals(false, mod.shouldBeOutput(false, 3).evaluate(a));
 		assertEquals(false, mod.shouldBeOutput(false, 4).evaluate(a));
 		assertEquals(false, mod.shouldBeOutput(false, 5).evaluate(a));
+	}
+	
+	@Test
+	public void testBackPorchValues1()
+	{
+		int Q_in = 6, Q_out = 6;
+		TrimModule mod = new TrimModule("dec", 3, s_domNumbers, Q_in, Q_out);
+		Assignment a = new Assignment();
+		mod.getFrontPorch(0).set(1, 2, 3, 1, 2, 3).assign(a);
+		mod.getResetPorch(0).getVariable().setValues(false, false, false, false, false, false).assign(a);
+		mod.getCounter().set(2).assign(a);
+		Condition c = mod.backPorchValues(false);
+		List<Assignment> solutions = s_solver.solveAll(c, a, mod.backPorchLength(false), mod.getBackPorch().isWellFormed());
+		assertEquals(1, solutions.size());
+		mod.getBackPorch().set(2, 3, 1, 2, 3).assign(a);
+		assertTrue(c.evaluate(a));
 	}
 }
